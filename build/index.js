cceb158a50290ee81288e9c4c5bda52dfc01ae1a
Renan Frozza <renan.apc@gmail.com>
2022-01-29 06:31:51 -0300

    Implementing the interface

--- build/index.js
+++ build/index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter");
 const NumbersCollection_1 = require("./NumbersCollection");
-const numberCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
+const numberCollection = new NumbersCollection_1.NumbersCollection([100, 3, -5, 0]);
 const sorte = new Sorter_1.Sorter(numberCollection);
 sorte.sort();
 console.log(numberCollection.data);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 
-const numberCollection = new NumbersCollection([10,3,-5,0]);
+const numberCollection = new NumbersCollection([100,3,-5,0]);
 const sorte = new Sorter(numberCollection);
 sorte.sort();
 console.log(numberCollection.data);
